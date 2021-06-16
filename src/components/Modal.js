2b92064a58131172094bf74d19f8e791a5d590af
Deryan <>
2021-06-16 14:51:16 +0700

    migrasi dari template ke components

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,5 @@
 function Modal({handleShowModal}) {
-    return ( 
+    return (
         <div className="fixed inset-0 z-40 flex items-center justify-center w-100 min-h-screen" onClick={handleShowModal}>
         <div className="fixed inset-0 bg-black opacity-35"></div>
         <div className="bg-white p-0 md:p-6 z-10">
@@ -7,8 +7,8 @@ function Modal({handleShowModal}) {
             <div className="absolute w-full h-full">
                 <iframe
                     title="Video"
-                    width="50%"
-                    height="50%"
+                    width="100%"
+                    height="100%"
                     src="https://www.youtube.com/embed/3h0_v1cdUIA"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
