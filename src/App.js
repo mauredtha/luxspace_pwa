408b46fcd60bd60b64af5b9aa033f94731322a45
Deryan <>
2021-06-24 11:14:07 +0700

    Add Splash Screen

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ import Clients from './components/Clients.js';
 import AsideMenu from './components/AsideMenu.js';
 import Footer from './components/Footer.js';
 import Offline from './components/Offline.js';
+import Splash from './pages/Splash.js';
 
 function App() {
 
   const [items, setItems] = React.useState([]);
 
   const [offlineStatus, setOfflineStatus] = React.useState(!navigator.onLine);
+
+  const [isLoading, setIsLoading] = React.useState(true);
 
   function handleOfflineStatus() {
     setOfflineStatus(!navigator.onLine);
@@ -41,6 +44,10 @@ function App() {
     window.addEventListener('online', handleOfflineStatus);
     window.addEventListener('offline', handleOfflineStatus);
 
+    setTimeout(function(){
+      setIsLoading(false);
+    }, 1500);
+
     return function() {
       window.removeEventListener('online', handleOfflineStatus);
       window.removeEventListener('offline', handleOfflineStatus);
@@ -51,14 +58,19 @@ function App() {
 
   return (
     <>
-    {offlineStatus &&  <Offline/>}
-    <Header />
-    <Hero />
-    <Browse />
-    <Arrive items={items} />
-    <Clients />
-    <AsideMenu />
-    <Footer />
+    {isLoading === true ? <Splash /> : 
+    (
+      <>
+      {offlineStatus &&  <Offline/>}
+      <Header />
+      <Hero />
+      <Browse />
+      <Arrive items={items} />
+      <Clients />
+      <AsideMenu />
+      <Footer />
+      </>
+    )}
     </>
   );
 }
